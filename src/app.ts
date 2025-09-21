6d3b33226bb3d5357f3f3e103ac06d292d160e54
Raphaelpimenta <raphael.pimentagomes@hotmail.com>
2025-09-21 06:18:45 -0300

    Positions cors

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,9 @@ import cors from "cors"
 function createApp () {
     const app = express()
 
+    app.use(cors())
     app.use(express.json());
     app.use("/api", router)
-    app.use(cors())
 
     return app
 }
